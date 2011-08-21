e81776aff8920f43bf0c7b0a1630181bba4cdfe8
Joe Heck <heckj@mac.com>
2011-08-20 21:00:42 -0700

    adding in view, cleaning up comments

---
+++ boardtest/GameBoardView.h
@@ -0,0 +1,13 @@
+//
+//  GameBoardView.h
+//  boardtest
+//
+//  Created by Joseph Heck on 8/20/11.
+//  Copyright (c) 2011 Nebula, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameBoardView : UIView
+
+@end
---
+++ boardtest/GameBoardView.m
@@ -0,0 +1,31 @@
+//
+//  GameBoardView.m
+//  boardtest
+//
+//  Created by Joseph Heck on 8/20/11.
+//  Copyright (c) 2011 Nebula, Inc. All rights reserved.
+//
+
+#import "GameBoardView.h"
+
+@implementation GameBoardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
